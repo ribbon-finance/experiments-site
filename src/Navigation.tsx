e24574e782a6e248bc280ec47eaa5f4e60bf5e58
Khoa Phan <khoaphan89@gmail.com>
2021-04-13 09:03:00 -0500

    Add button for connecting to MetaMask

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import { Navbar, NavbarBrand, Button } from "reactstrap";
 import logo from "./assets/logo.svg";
 
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
 const Navigation = () => {
+  const connect = () => {
+    if (!window.ethereum) {
+      return;
+    }
+
+    window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((accounts: string[]) => {
+        console.log(accounts);
+      })
+      .catch((err: Error) => {
+        // TO DO: Handle error
+        console.error(err);
+        return err;
+      });
+  };
+
   return (
-    <Navbar color="light" light expand="md">
+    <Navbar color="light" light expand>
       <NavbarBrand href="/">
-        <img src={logo} alt="Ribbon Finance logo" className="img-fluid" width={48} />
+        <img
+          src={logo}
+          alt="Ribbon Finance logo"
+          className="img-fluid"
+          width={48}
+        />
       </NavbarBrand>
+      <Button className="ml-auto" color="primary" onClick={connect}>
+        Connect MetaMask
+      </Button>
     </Navbar>
   );
 };
