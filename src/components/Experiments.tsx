c0c05cd1ec800f34925550dfdb82f993bc204155
Khoa Phan <khoaphan89@gmail.com>
2021-04-14 09:45:53 -0500

    Add initial home page implementation

---
+++ src/components/Experiments.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../static/css/Experiments.css";
+
+const Experiments = () => {
+  return (
+    <Container className="experiments-container">
+      <Row>
+        <Col className="experiments-column">
+          <div>
+            <h3>
+              <div className="emoji">🧪</div> Experimental
+            </h3>
+            <ul className="experiments-list">
+              <li className="experiments-list-item">
+                <div className="emoji">🚀💸</div> Up Only WBTC
+              </li>
+              <li className="experiments-list-item">
+                <div className="emoji">⏰🏦</div> WBTC Theta Vault
+              </li>
+              <li className="experiments-list-item">
+                <div className="emoji">🎢🤑</div> Vega Vault
+              </li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Experiments;
