344063954517c9ffd2f560fdd156a1a718a58b8b
Khoa Phan <9170866+kphed@users.noreply.github.com>
2021-04-15 10:16:23 -0500

    Front page (#4)
    * Add reactstrap
    
    * Use arrow function assignment instead of function declaration
    
    * Finish setting up reactstrap
    
    * Add Ribbon Finance logo to navigation
    
    * Add button for connecting to MetaMask
    
    * Implement organized file hierarchy
    
    * Remove light attributes
    
    * Remove text centering styling
    
    * Remove CRA boilerplate
    
    * Add initial home page implementation
    
    * Test vercel

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from './Navigation';
+import Experiments from './Experiments';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navigation />
+      <Experiments />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Navbar, NavbarBrand, Button } from "reactstrap";
+import logo from "../static/images/logo.svg";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+const Navigation = () => {
+  const connect = () => {
+    if (!window.ethereum) {
+      return;
+    }
+
+    window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((accounts: string[]) => {
+        console.log(accounts);
+      })
+      .catch((err: Error) => {
+        // TO DO: Handle error
+        console.error(err);
+        return err;
+      });
+  };
+
+  return (
+    <Navbar expand>
+      <NavbarBrand href="/">
+        <img
+          src={logo}
+          alt="Ribbon Finance logo"
+          className="img-fluid"
+          width={48}
+        />
+      </NavbarBrand>
+      <Button className="ml-auto" color="primary" onClick={connect}>
+        Connect MetaMask
+      </Button>
+    </Navbar>
+  );
+};
+
+export default Navigation;
