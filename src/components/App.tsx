d835a9e3d13bd593d26c03f84869df15e446e0c6
gg-salo <36091722+goncalopr@users.noreply.github.com>
2021-04-15 18:30:51 +0100

    added homepage and experiment detail layout with basic UI; app routing using react-router-dom; created a new component to dynamically generate list of strategies; CSS and basic styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navigation from './Navigation';
 import Experiments from './Experiments';
+// import ExperimentsList from './ExperimentsList';
+import ExperimentDetail from './ExperimentDetail'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Navigation />
-      <Experiments />
-    </div>
+      <Route exact path="/" component={ Experiments } />
+      <Route path="/up-only" component={ ExperimentDetail } />
+    </Router>
   );
 };
 
