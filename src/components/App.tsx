536405f94254c9f533f208df99bf54b521c25796
gg-salo <gg_salo@protonmail.com>
2021-04-21 11:14:19 +0100

    commit again for vercel to load properly

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navigation from './Navigation';
 import Experiments from './Experiments';
 import CreateCanvas from './CreateCanvas'
-// import ExperimentsList from './ExperimentsList';
 import ExperimentDetail from './ExperimentDetail';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
