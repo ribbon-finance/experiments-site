28c36c8b2412f2db945e7481fcc03f132029ae8b
gg-salo <gg_salo@protonmail.com>
2021-05-11 22:52:13 +0100

    fix merge conflicts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from './Navigation';
 import Experiments from './Experiments';
+import CreateCanvas from './CreateCanvas'
 import ExperimentDetail from './ExperimentDetail';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ const App = () => {
       <Navigation />
       <Route exact path="/" component={ Experiments } />
       <Route path="/up-only" component={ ExperimentDetail } />
+      <Route path="/canvas" component={ CreateCanvas } />
     </Router>
   );
 };
