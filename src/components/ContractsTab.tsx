05ed6377cba617d785242ba3ddcbfecaa18f8f6f
Ken Chan <guanhao3797@gmail.com>
2021-05-12 21:18:34 +0100

    Alfa (#7)
    * added homepage and experiment detail layout with basic UI; app routing using react-router-dom; created a new component to dynamically generate list of strategies; CSS and basic styling
    
    * fix of ExperimentDetail Component and basic structure for detail of experiments
    
    * Fixed typos and button interaction bugs. Finished experiment detail layout template - usability layout finished
    
    * Refactored tabs programatically; started lava exp
    
    * commit again for vercel to load properly
    
    * final styling homepage and detail page;
    
    * components for tabs; removed uneeded files and spacing
    
    * fix merge conflicts
    
    * final tweak merge conflicts
    
    Co-authored-by: gg-salo <36091722+goncalopr@users.noreply.github.com>
    Co-authored-by: gg-salo <gg_salo@protonmail.com>

---
+++ src/components/ContractsTab.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Row, Col, Card, CardBody } from 'reactstrap';
+
+const ContractsTab = () => {
+  return (
+    <div>
+      <Row>
+        <Col sm="12">
+          <Card className="mini-card">
+            <CardBody>
+              <Row className="content-section">
+                <Col sm="12">
+                  <div className="account">
+                    <div className="main-text">
+                      <strong>Your Account:</strong> 0x0000000000000000000
+                    </div>
+                  </div>
+                  <div className="vault-shares">
+                    <div className="main-text">
+                      <strong>Your Vault Shares:</strong> 0
+                    </div>
+                  </div>
+                  <div className="share-value">
+                    <div className="main-text">
+                      <strong>Shares Value:</strong> 0
+                    </div>
+                  </div>
+                  <div className="asset-balance">
+                    <div className="main-text">
+                      <strong>Your WBTC Balance:</strong> 0
+                    </div>
+                  </div>
+                  <div className="asset-balance">
+                    <div className="main-text">
+                      <strong>Your ETH Balance:</strong> 0
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default ContractsTab;
--- src/components/Experiments.tsx
+++ src/components/Experiments.tsx
@@ -3,28 +3,29 @@ import { Container, Row, Col, Button, ListGroup, ListGroupItem, Card } from "rea
 import "../static/css/Experiments.css";
 
 const Experiments = () => {
+
+  const data = [
+    { id: "1", emojis: "🚀💸", name:"Up Only WBTC" },
+    { id: "2", emojis: "⏰🏦", name:"WBTC Theta Vault" },
+    { id: "3", emojis: "🎢🤑", name:"Vega Vault" }
+  ];
+
   return (
     <Container className="experiments-container container min-vh-100">
       <Row className="row h-100 justify-content-center align-items-center">
         <Col className="experiments-column col-12">
           <div>
-            <h3>
+            <h3 className="title-section">
               <div className="emoji">🧪</div> Experimental
             </h3>
             <Card>
               <ListGroup className="experiments-list">
-                <ListGroupItem className="experiments-list-item">
-                  <div className="emoji">🚀💸</div> Up Only WBTC
-                  <Button className="float-right" color="primary" href="/up-only" size="sm"> <strong>+</strong> </Button>
-                </ListGroupItem>
-                <ListGroupItem className="experiments-list-item">
-                  <div className="emoji">⏰🏦</div> WBTC Theta Vault
-                  <Button className="float-right" color="primary" href="#" size="sm"> <strong>+</strong> </Button>
-                </ListGroupItem>
-                <ListGroupItem className="experiments-list-item">
-                  <div className="emoji">🎢🤑</div> Vega Vault
-                  <Button className="float-right" color="primary" href="#" size="sm"> <strong>+</strong> </Button>
-                </ListGroupItem>
+                {data.map(item => (
+                  <ListGroupItem className="experiments-list-item">
+                    <div className="emoji">{item.emojis}</div>- {item.name}
+                    <Button className="list-button float-right" color="primary" href="/up-only" size="sm"> <strong>+</strong> </Button>
+                  </ListGroupItem>
+                ))}
               </ListGroup>
             </Card>
 
